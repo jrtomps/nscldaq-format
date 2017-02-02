d84b452d8ad276220fa87074fffa7fe443b7dad5
Jeromy Tompkins <tompkins@nscl.msu.edu>
2017-02-02 16:17:52 -0500

    Adds format_cast

---
+++ format/V12/format_cast.h
@@ -0,0 +1,28 @@
+#ifndef DAQ_V12_FORMAT_CAST_H
+#define DAQ_V12_FORMAT_CAST_H
+
+#include <V12/CRawRingItem.h>
+#include <V12/CRingItem.h>
+#include <typeinfo>
+#include <iostream>
+
+namespace DAQ {
+namespace V12 {
+
+
+template<class T>
+T format_cast(const CRingItem& item) {
+    auto& rawItem = dynamic_cast<const CRawRingItem&>(item);
+    return T(rawItem);
+}
+
+template<> CRawRingItem format_cast(const CRingItem& item) {
+    CRawRingItem rawItem;
+    item.toRawRingItem(rawItem);
+    return rawItem;
+}
+
+} // end V12
+} // end DAQ
+
+#endif // DAQ_V12_FORMAT_CAST_H
