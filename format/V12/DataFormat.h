64daae0aad5f4b7ae7f23798c0d56e0436612803
Jeromy Tompkins <tompkins@nscl.msu.edu>
2017-02-03 08:54:53 -0500

    Updates author information

--- format/V12/DataFormat.h
+++ format/V12/DataFormat.h
@@ -3,7 +3,7 @@
 
 /*
     This software is Copyright by the Board of Trustees of Michigan
-    State University (c) Copyright 2005.
+    State University (c) Copyright 2017.
 
     You may use this software under the terms of the GNU public license
     (GPL).  The terms of this license are described at:
@@ -11,8 +11,9 @@
      http://www.gnu.org/licenses/gpl.txt
 
      Author:
-             Ron Fox
-	     NSCL
+    Jeromy Tompkins
+    Ron Fox
+         NSCL
 	     Michigan State University
 	     East Lansing, MI 48824-1321
 */
