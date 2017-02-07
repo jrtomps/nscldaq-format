01831319e3b4630ef30dd486fb050a1d05ba6ee3
Jeromy Tompkins <tompkins@nscl.msu.edu>
2017-02-07 14:44:48 -0500

    Reworks FormattedIO tests to work on std::stringstream

--- FormattedIO/daq11test.cpp
+++ FormattedIO/daq11test.cpp
@@ -19,7 +19,7 @@ static const char* Copyright = "(C) Copyright Michigan State University 2014, Al
 #include <fstream>
 #include <cppunit/extensions/HelperMacros.h>
 
-#include <CTestSourceSink.h>
+//#include <CTestSourceSink.h>
 #include <RingIOV11.h>
 #include <V11/CRingItem.h>
 #include <V11/CRingFragmentItem.h>
@@ -27,6 +27,7 @@ static const char* Copyright = "(C) Copyright Michigan State University 2014, Al
 #include <cstdint>
 #include <vector>
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 using namespace DAQ;
@@ -69,10 +70,10 @@ void CFormattedIOV11Test::tearDown()
 
 void CFormattedIOV11Test::input_0()
 {
-  CTestSourceSink ss(128);
+  std::stringstream ss;
   // Load the source/sink with data 
   const uint32_t data[] = {28, 30, 0, 0, 1, 2, 3};
-  ss.put(data, sizeof(data));
+  ss.write(reinterpret_cast<const char*>(data), sizeof(data));
 
   // extract a complete ring item
   V11::CRingItem item(1);
@@ -92,10 +93,10 @@ void CFormattedIOV11Test::input_0()
 
 void CFormattedIOV11Test::input_1()
 {
-  CTestSourceSink ss(128);
+  std::stringstream ss;
   // Load the source/sink with data (i.e item with body header) 
   const uint32_t data[] = {36, 30, 20, 1, 0, 2, 3, 0, 0};
-  ss.put(data, sizeof(data));
+  ss.write(reinterpret_cast<const char*>(data), sizeof(data));
 
   // extract a complete ring item
   V11::CRingItem item(1);
@@ -118,15 +119,13 @@ void CFormattedIOV11Test::input_1()
 
 void CFormattedIOV11Test::output_0()
 {
-  CTestSourceSink sink(128);
-
+  std::stringstream sink;
   vector<uint8_t> payload = {0, 1, 2, 3, 4, 5, 6};
   V11::CRingFragmentItem item(100, 2, payload.size(), payload.data(), 2);
 
   sink << item;
 
-  auto record = sink.getBuffer();
-
+  std::string record = sink.str();
   uint8_t* pOrig = reinterpret_cast<uint8_t*>(item.getItemPointer());
 
   CPPUNIT_ASSERT_EQUAL_MESSAGE("Number of bytes written are correct",
@@ -137,4 +136,75 @@ void CFormattedIOV11Test::output_0()
 
 
 }
+
+//void CFormattedIOV11Test::input_0()
+//{
+//  CTestSourceSink ss(128);
+//  // Load the source/sink with data
+//  const uint32_t data[] = {28, 30, 0, 0, 1, 2, 3};
+//  ss.put(data, sizeof(data));
+
+//  // extract a complete ring item
+//  V11::CRingItem item(1);
+//  ss >> item;
+
+//  CPPUNIT_ASSERT_EQUAL_MESSAGE("Size after extraction should be correct",
+//                               uint32_t(28), item.size());
+
+//  CPPUNIT_ASSERT_EQUAL_MESSAGE("Extracted type should be correct",
+//                               uint32_t(30), item.type());
+
+//  auto pBody = reinterpret_cast<uint32_t*>(item.getBodyPointer());
+
+//  CPPUNIT_ASSERT_MESSAGE("Body of extracted should be correct",
+//                          equal(pBody, pBody+4, data+3));
+//}
+
+//void CFormattedIOV11Test::input_1()
+//{
+//  CTestSourceSink ss(128);
+//  // Load the source/sink with data (i.e item with body header)
+//  const uint32_t data[] = {36, 30, 20, 1, 0, 2, 3, 0, 0};
+//  ss.put(data, sizeof(data));
+
+//  // extract a complete ring item
+//  V11::CRingItem item(1);
+//  ss >> item;
+
+//  CPPUNIT_ASSERT_EQUAL_MESSAGE("Size after extraction should be correct",
+//                               uint32_t(36), item.size());
+
+//  CPPUNIT_ASSERT_EQUAL_MESSAGE("Extracted type should be correct",
+//                               uint32_t(30), item.type());
+
+//  auto pBody = reinterpret_cast<uint32_t*>(item.getBodyPointer());
+
+//  CPPUNIT_ASSERT_MESSAGE("Body of extracted should be correct",
+//                          equal(pBody, pBody, data+7));
+
+//  CPPUNIT_ASSERT_EQUAL_MESSAGE("Body size should be correct",
+//                               size_t(8), item.getBodySize());
+//}
+
+//void CFormattedIOV11Test::output_0()
+//{
+//  CTestSourceSink sink(128);
+
+//  vector<uint8_t> payload = {0, 1, 2, 3, 4, 5, 6};
+//  V11::CRingFragmentItem item(100, 2, payload.size(), payload.data(), 2);
+
+//  sink << item;
+
+//  auto record = sink.getBuffer();
+
+//  uint8_t* pOrig = reinterpret_cast<uint8_t*>(item.getItemPointer());
+
+//  CPPUNIT_ASSERT_EQUAL_MESSAGE("Number of bytes written are correct",
+//                               size_t(item.size()), record.size());
+
+//  CPPUNIT_ASSERT_MESSAGE("What was written actually was written",
+//                          equal(record.begin(), record.end(), pOrig));
+
+
+//}
 
